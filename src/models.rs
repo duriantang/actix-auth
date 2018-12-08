a0c41a7ee70bc905ba336b26d1ac113525d0ec8b
Hui Tang <duriantang@gmail.com>
2018-12-08 18:21:40 +0800

    use uuid ~0.6 solver impl problem

--- src/models.rs
+++ src/models.rs
@@ -1,8 +1,11 @@
+use crate::schema::{invitations, users};
 use actix::{Actor, SyncContext};
+use chrono::NaiveDateTime;
 use diesel::{
     pg::PgConnection,
     r2d2::{ConnectionManager, Pool},
 };
+use uuid::Uuid;
 
 type PgPool = Pool<ConnectionManager<PgConnection>>;
 
@@ -12,3 +15,25 @@ impl Actor for DbExecutor {
     type Context = SyncContext<Self>;
 }
 
+#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
+#[table_name = "users"]
+pub struct User {
+    pub email: String,
+    pub password: String,
+    pub created_at: NaiveDateTime,
+}
+
+impl User {
+    pub fn remove_pwd(mut self) -> Self {
+        self.password = "".to_string();
+        self
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
+#[table_name = "invitations"]
+pub struct Invitation {
+    pub id: Uuid,
+    pub email: String,
+    pub expires_at: NaiveDateTime,
+}
