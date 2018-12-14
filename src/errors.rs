6d9f7e978da8fdb8c7fca495e07ed55983795955
Hui Tang <duriantang@gmail.com>
2018-12-14 17:56:55 +0800

    improve ServiceError

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,7 @@
 use actix_web::{error::ResponseError, HttpResponse};
+use diesel::result::{DatabaseErrorKind, Error};
+use std::convert::From;
+use uuid::ParseError;
 
 #[derive(Fail, Debug)]
 pub enum ServiceError {
@@ -7,15 +10,39 @@ pub enum ServiceError {
 
     #[fail(display = "BadRequest: {}", _0)]
     BadRequest(String),
+
+    #[fail(display = "Unauthorized")]
+    Unauthorized,
 }
 
 impl ResponseError for ServiceError {
     fn error_response(&self) -> HttpResponse {
         match *self {
-            ServiceError::InternalServerError => {
-                HttpResponse::InternalServerError().json("Internal Server Error")
-            }
+            ServiceError::InternalServerError => HttpResponse::InternalServerError()
+                .json("Internal Server Error, Please try again later."),
             ServiceError::BadRequest(ref message) => HttpResponse::BadRequest().json(message),
+            ServiceError::Unauthorized => HttpResponse::Unauthorized().json("Unauthorized"),
+        }
+    }
+}
+
+impl From<ParseError> for ServiceError {
+    fn from(_: ParseError) -> ServiceError {
+        ServiceError::BadRequest("Invalid UUID".into())
+    }
+}
+
+impl From<Error> for ServiceError {
+    fn from(error: Error) -> ServiceError {
+        match error {
+            Error::DatabaseError(kind, info) => {
+                if let DatabaseErrorKind::UniqueViolation = kind {
+                    let message = info.details().unwrap_or_else(|| info.message()).to_string();
+                    return ServiceError::BadRequest(message);
+                }
+                ServiceError::InternalServerError
+            }
+            _ => ServiceError::InternalServerError,
         }
     }
 }
